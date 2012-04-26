54a3464004ed1259e2d6b965552e982bb46308e9
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-26 23:29:42 +0000

    cleaned up
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@61 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- lib/errors/destroy_with_references_error.rb
+++ lib/errors/destroy_with_references_error.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 class DestroyWithReferencesError < StandardError
 
   def initialize(reference_name, reference_count)
-    puts "Im here"
     super "Unable to delete: record is referenced by #{reference_count} #{reference_name} " + 'record'.pluralize(reference_count)
   end
 
