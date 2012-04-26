b0f5df7b91ba5ceca83b5070965cc7b1de7e4965
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-26 21:07:43 +0000

    cleaned up code
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@52 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- lib/gems/ucb_rails_security-3.0.0/init.rb
+++ lib/gems/ucb_rails_security-3.0.0/init.rb
@@ -5,5 +5,5 @@ require 'ucb_rs_controller_methods'
 require 'helpers/rspec_helpers'
 require 'ucb_rails_security_logger'
 
-UCB::Rails::Security.logger = UCB::Rails::Security::Logger.new("#{Rails.root}/log/ucb_security_#{Rails.env}.log")
+UCB::Rails::Security.logger = UCB::Rails::Security::Logger.new(Rails.root + "/log/ucb_security_#{Rails.env}.log")
 UCB::Rails::Security.logger.formatter = UCB::Rails::Security::Logger::Formatter.new
--- lib/gems/ucb_rails_security-3.0.0/lib/ucb_rails_security.rb
+++ lib/gems/ucb_rails_security-3.0.0/lib/ucb_rails_security.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'rails'
 
 begin
   require 'action_pack'
@@ -16,7 +17,7 @@ require 'ucb_ldap'
 
 require 'helpers/rspec_helpers'
 require 'ucb_rails_security_casauthentication'
-require 'ucb_rs_controller_methods' 
+require 'ucb_rs_controller_methods'
 require 'ucb_rails_security_logger'
 
 
@@ -24,29 +25,29 @@ module UCB #:nodoc:
   module Rails #:nodoc:
     # = UCB Rails Security
     module Security
-      
+
       # Returns +true+ if application is using a user table 
       # which must be named +User+.
       def self.using_user_table?
         @using_user_table
       end
-      
+
       # Set this to +true+ if your application is using a +User+ table.
       def self.using_user_table=(bool)
         @using_user_table = bool
       end
-      
+
       # URL user is redirected to when authorization fails. 
       # Defaults to '/not_authorized'
       def self.not_authorized_url()
         @not_authorized_url || '/not_authorized'
       end
-      
+
       # Setter for not_authorized_url()
       def self.not_authorized_url=(url)
         @not_authorized_url = url
       end
-      
+
       # Reset instance variables for testing
       def self.reset_instance_variables() #:nodoc:
         self.using_user_table = nil
@@ -54,9 +55,9 @@ module UCB #:nodoc:
       end
 
       def self.logger
-        @logger ||= UCB::Rails::Security::Logger.new("#{Rails.root}/log/ucb_security_#{Rails.env}.log")
+        @logger ||= UCB::Rails::Security::Logger.new("#{::Rails.root}/log/ucb_security_#{::Rails.env}.log")
       end
-      
+
       def self.logger=(val)
         @logger = val
       end
