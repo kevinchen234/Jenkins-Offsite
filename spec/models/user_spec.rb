a50a708764626b953cd6fa2d7b372c42a72a788b
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-26 21:11:05 +0000

    User spec completed
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@58 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,6 @@ describe "User" do
   fixtures :users
   fixtures :off_site_requests
 
-  #before(:each) do
-  #  @user = users(:runner)
-  #end
-
   describe "Require attributes" do
 
     it "requires ldap_uid" do
@@ -220,89 +216,3 @@ describe "User" do
 
 end
 
-=begin
-
-
-
-  it "should allow a user to be deleted if it is not referenced by other records" do
-    runner = users(:runner)
-    runner.off_site_requests.clear
-    runner.save!
-    runner.off_site_requests.should be_empty
-    runner.destroy.should be_true
-  end
-=end
-#-------------------
-
-
-__END__
-
-
-describe User do
-  fixtures(:all)
-  
-  before(:each) do
-    @admin = users(:admin)
-  end
-
-  it "should assign all attributes (including protected) when sent protected_attributes=" do
-    user = User.new
-    attrs = @admin.attributes.delete_if { |k, v| ["id", "updated_at", "created_at"].include?(k) }
-    user.protected_attributes = attrs
-    attrs.each { |k, v| user.send(k).should == v }
-  end
-
-  it "should find by filter when snt #find_from_filter(filter)" do
-    # disabled filter
-    disabled = User.find_from_filter("disabled")
-    disabled.should have(1).user
-    disabled[0].email.should == "disabled@berkeley.edu"
-
-    # buyer filter
-    buyers = User.find_from_filter("buyers")
-    buyers.should have(2).users
-    buyers.each { |p| p.roles.map(&:name).include?("buyer").should be_true }
-    
-    # admin filter
-    admins = User.find_from_filter("admins")
-    admins.should have(2).users
-    admins.each { |p| p.roles.map(&:name).include?("admin").should be_true }
-
-    # Any filter where FILTER !IN FILTER_BY_OPTIONS returns all users
-    all = User.find_from_filter("")
-    all.should have(7).users
-
-    all = User.find_from_filter(nil)
-    all.should have(7).users
-    
-    all = User.find_from_filter("invalid filter")
-    all.should have(7).users
-  end
-
-  it "should instantiate new user when sent #new_from_ldap_uid" do
-    # user if found, copy attributes over
-    user = User.new_from_ldap_uid(@admin.ldap_uid)
-    user.new_record?.should be_true
-    user.ldap_uid.should == @admin.ldap_uid
-
-    # user is not found, return new untainted user
-    user = User.new_from_ldap_uid(nil)
-    user.new_record?.should be_true
-    user.ldap_uid.should be_nil
-  end
-
-  it "should not allow a user to be deleted if it is referenced by other records" do
-    runner = users(:runner)
-    runner.off_site_requests.should have(2).records
-    runner.destroy.should be_false
-  end
-
-  it "should allow a user to be deleted if it is not referenced by other records" do
-    runner = users(:runner)
-    runner.off_site_requests.clear
-    runner.save!
-    runner.off_site_requests.should be_empty
-    runner.destroy.should be_true
-  end
-
-end
