dfe202f38796a943f016f9b6389570d17dfadb85
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-30 07:15:39 +0000

    ext_circumstance model test rewrite
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@62 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- spec/models/ext_circumstance_spec.rb
+++ spec/models/ext_circumstance_spec.rb
@@ -1,21 +1,28 @@
 require 'spec_helper'
 
-describe ExtCircumstance, "referential integrity" do
+
+describe "#destroy has referential integrity" do
   fixtures :off_site_requests, :ext_circumstances
-  before(:each) do
-    @ext_cir = ext_circumstances(:ext_one)
-    @req = off_site_requests(:runner_request_1)
+
+  it "should not allow ext circumstances to be deleted if referenced by other records" do
+    ext_cir = ext_circumstances(:ext_one)
+    osr = off_site_requests(:minimal_for_validation)
+    ext_cir.off_site_requests << osr
+    ext_cir.save!
+    ext_cir.off_site_requests.should have(1).records
+    lambda {ext_cir.destroy}.should raise_error DestroyWithReferencesError
   end
 
-  it "should not be destroyed if it is referenced by other records" do
-    @ext_cir.off_site_requests.should have(2).records
-    @ext_cir.destroy.should be_false
-
-    @ext_cir.off_site_requests.clear
-    @ext_cir.save!
-    @ext_cir.off_site_requests.should be_empty
-    @ext_cir.destroy.should be_true
+  it "should allow an ext circumstance to be deleted if it is not referenced by other records" do
+    ext_cir = ext_circumstances(:ext_one)
+    ext_cir.off_site_requests.clear
+    ext_cir.save!
+    ext_cir.off_site_requests.should be_empty
+    ext_cir.destroy.should be_true
   end
+
+
 
 end
+
 
