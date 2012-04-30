1ecaf598c3947f0a1c1442a8de9779fd3eb054c2
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-30 22:46:01 +0000

    ext_circumstance model test rewrite
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@63 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- spec/models/ext_circumstance_spec.rb
+++ spec/models/ext_circumstance_spec.rb
@@ -5,7 +5,7 @@ describe "#destroy has referential integrity" do
   fixtures :off_site_requests, :ext_circumstances
 
   it "should not allow ext circumstances to be deleted if referenced by other records" do
-    ext_cir = ext_circumstances(:ext_one)
+    ext_cir = ext_circumstances(:minimal_for_validation)
     osr = off_site_requests(:minimal_for_validation)
     ext_cir.off_site_requests << osr
     ext_cir.save!
@@ -14,7 +14,7 @@ describe "#destroy has referential integrity" do
   end
 
   it "should allow an ext circumstance to be deleted if it is not referenced by other records" do
-    ext_cir = ext_circumstances(:ext_one)
+    ext_cir = ext_circumstances(:minimal_for_validation)
     ext_cir.off_site_requests.clear
     ext_cir.save!
     ext_cir.off_site_requests.should be_empty
