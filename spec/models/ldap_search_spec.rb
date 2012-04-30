da33938d79442c868af68a60f485bba99f5a5cf1
vnadi <vnadi@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-04-30 22:52:25 +0000

    Complete ldap_search spec; it runs as is, and we merely made the values generic
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@64 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- spec/models/ldap_search_spec.rb
+++ spec/models/ldap_search_spec.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe LdapSearch do
   
   it "should create filter" do
-    LdapSearch.create_filter(:search_by => 'ldap_uid', :search_value => 'uid').should == {:uid=>"uid"}
-    LdapSearch.create_filter(:search_by => 'last_first_name', :search_value => 'Last, First').should == {:givenname=>"First", :sn=> "Last"}
-    LdapSearch.create_filter(:search_by => 'last_name', :search_value => 'Last').should == { :sn => "Last" }
-    LdapSearch.create_filter(:search_by => 'first_name', :search_value => 'first').should == { :givenname => "first" }
+    LdapSearch.create_filter(:search_by => 'ldap_uid', :search_value => 'foo').should == {:uid=>"foo"}
+    LdapSearch.create_filter(:search_by => 'last_first_name', :search_value => 'bar, foo').should == {:givenname=>"foo", :sn=> "bar"}
+    LdapSearch.create_filter(:search_by => 'last_name', :search_value => 'foo').should == { :sn => "foo" }
+    LdapSearch.create_filter(:search_by => 'first_name', :search_value => 'foo').should == { :givenname => "foo" }
     LdapSearch.create_filter(:search_by => 'email', :search_value => 'foo').should == { :mail => 'foo' }
   end
 
