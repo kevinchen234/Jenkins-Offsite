9d6b1e6630290d54ccd6ac09f250fea5ec35632d
nathanconnor <nathanconnor@08f60cee-26c5-4d95-bd4d-90821f7c72d7>
2012-07-18 21:49:41 +0000

    Committing new schema to overwrite previous wrong one. Should still run migrations and then rake db:schema:dump instead though
    git-svn-id: svn+ssh://code.berkeley.edu/istcts/offsite_v3/trunk@102 08f60cee-26c5-4d95-bd4d-90821f7c72d7

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100906210716) do
+ActiveRecord::Schema.define(:version => 20120716200154) do
 
   create_table "app_heartbeats", :force => true do |t|
     t.string "name"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(:version => 20100906210716) do
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
     t.text     "comment"
+    t.string   "additional_DNS_instructions"
   end
 
   create_table "roles", :force => true do |t|
@@ -78,11 +79,11 @@ ActiveRecord::Schema.define(:version => 20100906210716) do
     t.datetime "updated_at",                   :null => false
   end
 
-  create_table "user_roles", :id => false, :force => true do |t|
+  create_table "user_roles", :force => true do |t|
     t.integer  "user_id"
     t.integer  "role_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
